41261078eae4e57e7776fc2770485249dd9e00b6
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-07-14 17:00:58 +0300

    Change booking form

--- site/src/components/Booking.js
+++ site/src/components/Booking.js
@@ -1,80 +1,13 @@
 export default function Booking() {
     return (
         <>
-            <div id="search" style={{ background: "#f2f2f2;" }}>
-                <div class="container">
-                    <div class="form-row">
-                        <div class="control-group col-md-3">
-                            <label>Check-In</label>
-                            <div class="form-group">
-                                <div class="input-group date" id="date-3" data-target-input="nearest">
-                                    <input type="text" class="form-control datetimepicker-input" data-target="#date-3" />
-                                    <div class="input-group-append" data-target="#date-3" data-toggle="datetimepicker">
-                                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="control-group col-md-3">
-                            <label>Check-Out</label>
-                            <div class="form-group">
-                                <div class="input-group date" id="date-4" data-target-input="nearest">
-                                    <input type="text" class="form-control datetimepicker-input" data-target="#date-4" />
-                                    <div class="input-group-append" data-target="#date-4" data-toggle="datetimepicker">
-                                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="control-group col-md-3">
-                            <div class="form-row">
-                                <div class="control-group col-md-6">
-                                    <label>Adult</label>
-                                    <select class="custom-select">
-                                        <option selected>0</option>
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
-                                        <option value="6">6</option>
-                                        <option value="7">7</option>
-                                        <option value="8">8</option>
-                                        <option value="9">9</option>
-                                        <option value="10">10</option>
-                                    </select>
-                                </div>
-                                <div class="control-group col-md-6 kid">
-                                    <label>Kid</label>
-                                    <select class="custom-select">
-                                        <option selected>0</option>
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
-                                        <option value="6">6</option>
-                                        <option value="7">7</option>
-                                        <option value="8">8</option>
-                                        <option value="9">9</option>
-                                        <option value="10">10</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="control-group col-md-3">
-                            <button class="btn btn-block">Search</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <div id="booking">
                 <div class="container">
                     <div class="section-header">
-                        <h2>Room Booking</h2>
+                        <h2>Направете резервация</h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in mi libero. Quisque convallis, enim at venenatis tincidunt.
+                            От тук може да направите резервация. <br /> <b>ВАЖНО!!!</b> Ако не сте записали желаните дати в менюто "Стаи", е възможно да няма свободни стаи от типа, който искате!
                         </p>
                     </div>
                     <div class="row">
@@ -84,46 +17,56 @@ export default function Booking() {
                                 <form name="sentMessage" id="bookingForm" novalidate="novalidate">
                                     <div class="form-row">
                                         <div class="control-group col-md-6">
-                                            <label>First Name</label>
-                                            <input type="text" class="form-control" id="fname" placeholder="E.g. John" required="required" data-validation-required-message="Please enter first name" />
+                                            <label>Име: </label>
+                                            <input type="text" class="form-control" id="fname" placeholder="Петър" required="required" data-validation-required-message="Моля, въведете име!" />
                                             <p class="help-block text-danger"></p>
                                         </div>
                                         <div class="control-group col-md-6">
-                                            <label>Last Name</label>
-                                            <input type="text" class="form-control" id="lname" placeholder="E.g. Sina" required="required" data-validation-required-message="Please enter last name" />
-                                            <p class="help-block text-danger"></p>
-                                        </div>
-                                    </div>
-                                    <div class="form-row">
-                                        <div class="control-group col-md-6">
-                                            <label>Mobile</label>
-                                            <input type="text" class="form-control" id="mobile" placeholder="E.g. +1 234 567 8900" required="required" data-validation-required-message="Please enter your mobile number" />
-                                            <p class="help-block text-danger"></p>
-                                        </div>
-                                        <div class="control-group col-md-6">
-                                            <label>Email</label>
-                                            <input type="email" class="form-control" id="email" placeholder="E.g. email@example.com" required="required" data-validation-required-message="Please enter your email" />
+                                            <label>Фамилия: </label>
+                                            <input type="text" class="form-control" id="lname" placeholder="Иванов" required="required" data-validation-required-message="Моля, въведете фамилия!" />
                                             <p class="help-block text-danger"></p>
                                         </div>
                                     </div>
                                     <div class="form-row">
                                         <div class="control-group col-md-6">
-                                            <label>Check-In</label>
-                                            <input type="text" class="form-control" id="date-1" data-toggle="datetimepicker" data-target="#date-1" placeholder="E.g. MM/DD/YYYY" required="required" data-validation-required-message="Please enter date" />
+                                            <label>Телефонен номер:</label>
+                                            <input type="text" class="form-control" id="mobile" placeholder="0883125467" required="required" data-validation-required-message="Моля, въведете телефонен номер!" />
                                             <p class="help-block text-danger"></p>
                                         </div>
                                         <div class="control-group col-md-6">
-                                            <label>Check-Out</label>
-                                            <input type="text" class="form-control" id="date-2" data-toggle="datetimepicker" data-target="#date-2" placeholder="E.g. MM/DD/YYYY" required="required" data-validation-required-message="Please enter date" />
+                                            <label>Имейл:</label>
+                                            <input type="email" class="form-control" id="email" placeholder="email@example.com" required="required" data-validation-required-message="Моля, въведете имейл!" />
+                                            <p class="help-block text-danger"></p>
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="control-group col-md-6">
+                                            <label>Дата на настаняване:</label>
+                                            <input type="text" class="form-control" id="date-1" data-toggle="datetimepicker" data-target="#date-1" placeholder="MM/DD/YYYY" required="required" data-validation-required-message="Моля, въведете дата на настаняване!" />
+                                            <p class="help-block text-danger"></p>
+                                        </div>
+                                        <div class="control-group col-md-6">
+                                            <label>Дата на напускане:</label>
+                                            <input type="text" class="form-control" id="date-2" data-toggle="datetimepicker" data-target="#date-2" placeholder="MM/DD/YYYY" required="required" data-validation-required-message="Моля, въведете дата на напускане!" />
                                             <p class="help-block text-danger"></p>
                                         </div>
                                     </div>
                                     <div class="control-group">
-                                        <label>Special Request</label>
-                                        <input type="text" class="form-control" id="request" placeholder="E.g. Special Request" required="required" data-validation-required-message="Please enter your special request" />
+                                        <label>Брой гости:</label>
+                                        <select class="custom-select">
+                                            <option selected>1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                        </select>
                                         <p class="help-block text-danger"></p>
                                     </div>
-                                    <div class="button"><button type="submit" id="bookingButton">Book Now</button></div>
+                                    <div class="control-group">
+                                        <label>Допълнителни изисквания:</label>
+                                        <input type="text" class="form-control" id="request" placeholder="Допълнителни изисквания" required="required" data-validation-required-message="Моля, въведете Вашите допълнителни изисквания!" />
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="button"><button type="submit" id="bookingButton">Резервирай</button></div>
                                 </form>
                             </div>
                         </div>
--- site/src/services/getRooms.js
+++ site/src/services/getRooms.js
@@ -1,19 +1,53 @@
 import { db } from '../config/firebase';
-import { collection, query, getDocs } from 'firebase/firestore';
+import {
+    collection,
+    query,
+    getDocs,
+    where,
+} from 'firebase/firestore';
+import { Timestamp } from 'firebase/firestore';
 
-export async function getRooms() {
+// Основна функция
+export async function getRooms(checkIn = null, checkOut = null) {
     try {
-        const q = query(collection(db, "rooms"));
-        const querySnapshot = await getDocs(q);
-
-        const products = [];
-        querySnapshot.forEach((doc) => {
-            products.push({ id: doc.id, ...doc.data() });
+        const roomsSnapshot = await getDocs(query(collection(db, "rooms")));
+        const allRooms = [];
+        roomsSnapshot.forEach((doc) => {
+            allRooms.push({ id: doc.id, ...doc.data() });
         });
 
-        return products;
+        // Ако няма зададени дати, връщаме всички стаи
+        if (!checkIn || !checkOut) return allRooms;
+
+        // Преобразуване на датите в Firestore Timestamp
+        const checkInDate = Timestamp.fromDate(new Date(checkIn));
+        const checkOutDate = Timestamp.fromDate(new Date(checkOut));
+
+        // Взимаме всички резервации
+        const bookingsSnapshot = await getDocs(query(collection(db, "bookings")));
+        const bookings = [];
+        bookingsSnapshot.forEach(doc => bookings.push(doc.data()));
+
+        // Проверка за заетост на стаите
+        const availableRooms = allRooms.filter(room => {
+            const totalCount = parseInt(room.totalCount || "1");
+
+            // Филтрираме резервациите за тази стая
+            const bookingsForRoom = bookings.filter(b =>
+                b.roomType === room.type &&
+                (
+                    (b.checkIn.toDate() < checkOutDate.toDate()) &&
+                    (b.checkOut.toDate() > checkInDate.toDate())
+                )
+            );
+
+            return bookingsForRoom.length < totalCount;
+        });
+
+        return availableRooms;
+
     } catch (error) {
-        console.error("Грешка при извличане на задачи: ", error);
+        console.error("Грешка при извличане на стаи: ", error);
         return [];
     }
 }
