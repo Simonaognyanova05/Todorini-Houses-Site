44071b0003822f816ceec78853abe70b1d6441c4
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-07-17 22:52:06 +0300

    Change texts

--- site/src/components/Contact.js
+++ site/src/components/Contact.js
@@ -22,9 +22,9 @@ export default function Contact() {
         <div id="contact">
             <div class="container">
                 <div class="section-header">
-                    <h2>Contact</h2>
+                    <h2>Контакти</h2>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in mi libero. Quisque convallis, enim at venenatis tincidunt.
+                        Може да се свържете с нас като попълните формата по-долу или използвате имейл адреса и телефонния номер.
                     </p>
                 </div>
                 <div class="row">
@@ -32,17 +32,17 @@ export default function Contact() {
                         <div class="row contact-info">
                             <div class="col-md-4">
                                 <div class="info-item">
-                                    <p><i class="fa fa-map-marker"></i>22 Wolf Road, California</p>
+                                    <p><i class="fa fa-map-marker"></i>ул. "Любен Каравелов" 22</p>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="info-item">
-                                    <p><i class="fa fa-envelope"></i> <a href="mailto:info@example.com">info@example.com</a></p>
+                                    <p><i class="fa fa-envelope"></i> <a href="mailto:contacts@todorinikashti.com">contacts@todorinikashti.com</a></p>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="info-item">
-                                    <p><i class="fa fa-phone"></i><a href="tel:+1 234 567 8900">+1 234 567 8900</a></p>
+                                    <p><i class="fa fa-phone"></i><a href="tel:+1 234 567 8900">0887349901, 0887733430</a></p>
                                 </div>
                             </div>
                         </div>
@@ -53,27 +53,27 @@ export default function Contact() {
                             <form name="sentMessage" id="contactForm" novalidate="novalidate" onSubmit={sendHandler}>
                                 <div class="form-row">
                                     <div class="control-group col-sm-6">
-                                        <label>Your Name</label>
-                                        <input type="text" class="form-control" id="name" name="name" placeholder="E.g. John Sina" required="required" data-validation-required-message="Please enter your name" />
+                                        <label>Вашите имена: </label>
+                                        <input type="text" class="form-control" id="name" name="name" placeholder="Петър Иванов" required="required" data-validation-required-message="Please enter your name" />
                                         <p class="help-block text-danger"></p>
                                     </div>
                                     <div class="control-group col-sm-6">
-                                        <label>Email</label>
-                                        <input type="email" class="form-control" id="email" name="email" placeholder="E.g. email@example.com" required="required" data-validation-required-message="Please enter your email" />
+                                        <label>Имейл</label>
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="email@example.com" required="required" data-validation-required-message="Please enter your email" />
                                         <p class="help-block text-danger"></p>
                                     </div>
                                 </div>
                                 <div class="control-group">
-                                    <label>Subject</label>
-                                    <input type="text" class="form-control" id="subject" name="subject" placeholder="E.g. Room Booking" required="required" data-validation-required-message="Please enter a subject" />
+                                    <label>Тема</label>
+                                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Детско" required="required" data-validation-required-message="Please enter a subject" />
                                     <p class="help-block text-danger"></p>
                                 </div>
                                 <div class="control-group">
-                                    <label>Message</label>
-                                    <textarea class="form-control" id="message" name="message" rows="5" placeholder="E.g. I need a premium room" required="required" data-validation-required-message="Please enter your message"></textarea>
+                                    <label>Съобщение:</label>
+                                    <textarea class="form-control" id="message" name="message" rows="5" placeholder="Предлагате ли детско легло?" required="required" data-validation-required-message="Please enter your message"></textarea>
                                     <p class="help-block text-danger"></p>
                                 </div>
-                                <div class="button"><button type="submit" id="sendMessageButton">Send Message</button></div>
+                                <div class="button"><button type="submit" id="sendMessageButton">Изпрати съобщенето</button></div>
                             </form>
                         </div>
                     </div>
